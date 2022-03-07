ea45bb67e52e86c445a57fc7a4c3414e035107bd
Marino Missiroli <marino.missiroli@cern.ch>
2022-03-07 09:05:11 +0100

    Implemented prettyprint

--- PythonScript/configuration.py
+++ PythonScript/configuration.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from lxml import etree
+from tidylib import tidy_document
+from pathlib import Path
 
 hybrid_id = 1
 num_chips = 3
@@ -12,3 +14,7 @@ chip_element = chip.find("RD53")
 Hybrid.insert(1,chip_element)
 
 tag.write("result.xml",pretty_print=True)
+
+Result = Path("result.xml").read_text()
+document, errors = tidy_document(Result, options={'indent':'auto','indent-attributes':'yes', 'input-xml':'yes'})
+Path("result.xml").write_text(document)
